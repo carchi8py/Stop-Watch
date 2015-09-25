75530f8eee04d6de4b9c5cbd186ee49a6ec78df2
Chris Archibald <carchi8py@gmail.com>
2015-09-24 21:51:02 -0700

    add a reset on view load.

--- Stop Watch/ViewController.swift
+++ Stop Watch/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        timeLabel.text = dateStringFromTimeInterval(0)
     }
     
     func start() {
